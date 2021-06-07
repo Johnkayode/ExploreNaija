bfe880b9807e30fc71f9dab62ea9c938690014fc
nerdthejohn <newtonjohn043@gmail.com>
2021-06-07 18:04:13 +0100

    inshallah

--- exploreNaija/settings/local.py
+++ exploreNaija/settings/local.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'), os.path.join(BASE_DIR, 'staticfiles/')]
 
 
 REST_FRAMEWORK = {
