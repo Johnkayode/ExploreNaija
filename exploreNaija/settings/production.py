214c092a72208bc55b9fe2ec9315dfc3e92c48f5
E-wave112 <osagieiyayi09@gmail.com>
2021-06-08 15:50:55 +0000

    edited the dockerfile

--- exploreNaija/settings/production.py
+++ exploreNaija/settings/production.py
@@ -5,7 +5,7 @@ ALLOWED_HOSTS = ['explore-naija.herokuapp.com',"www.explore-naija.herokuapp.com"
 
 SECRET_KEY =  "johndidthis-07-06-2021)h^+_i5s^h$6bim2)p%5we=!3@ob*958o)ttkc$$pdll8=%-t)"
 
-DEBUG = True
+DEBUG = False
 
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
